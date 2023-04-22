6685375ed8692e7ca34f6a2b0a0106502de54361
Rene Dohan <dohan.rene@gmail.com>
2023-04-22 19:03:58 +0200

    ...

--- UnityPlayerMock/src/main/java/com/unity3d/player/UnityPlayer.java
+++
@@ -1,6 +0,0 @@
-package com.unity3d.player;
-
-public class UnityPlayer {
-    public static void UnitySendMessage(String gameObjectName, String onMidiInputDeviceAttached, String deviceAddress) {
-    }
-}
